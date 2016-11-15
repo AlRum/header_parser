5d88437192d1fd0045b736b71042f4e7a154c2d0
AlRum <shiroc5@gmail.com>
2016-11-15 19:36:36 +0000

    ver.1

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  var ip = req.headers["x-forwarded-for"]
+  var lang = req.headers["accept-language"]
+  var soft =req.headers["user-agent"]
+  var fin=JSON.stringify({ipadress:ip, language:lang, software:soft})
+  res.send(fin)
 })
 
-app.listen(3000, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 3000!')
 })
